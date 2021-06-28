bb6b4a75bbe1d5bb96e3aa144dc4ebd1768534fe
Vogeln7 <david.vogel.7@gmail.com>
2021-06-27 23:31:45 -0300

    Se modifica el precio aleatorio, se hace que el numero aleatorio sea entre 1 y 10, anteriormente enra entre 0 y 10
    Signed-off-by: Vogeln7 <david.vogel.7@gmail.com>

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const http = require(`http`);
 const PUERTO = 3000;
 
 let server = http.createServer((req,res)=>{
-    let numAleatorio = ()=>Math.floor(Math.random()*(10)); 
+    let numAleatorio = ()=>Math.floor(Math.random()*(10)+1); 
+    let precioAleatorio = ()=>(Math.random() * (10000 - 0)+0).toFixed(2)
     let producto = {
         id:numAleatorio(),
         title:`Producto: ${numAleatorio()}`,
-        precio: numAleatorio(),
+        precio: precioAleatorio(),
         thumbnail:`Foto: ${numAleatorio()}`
     }
     
